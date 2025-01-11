cbd0f3aad59ef269acc3c246521c6b9bd5c0db6d
leeys9423 <leeys9423@gmail.com>
2025-01-11 20:27:32 +0900

    feat: 게시판 글 등록 컨트롤러 메서드 추가

--- src/main/java/com/example/tdd/domain/controller/BoardController.java
+++ src/main/java/com/example/tdd/domain/controller/BoardController.java
@@ -1,8 +1,27 @@
 package com.example.tdd.domain.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.tdd.domain.dto.BoardCreateRequest;
+import com.example.tdd.domain.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/boards")
+@RequiredArgsConstructor
 public class BoardController {
 
+    private final BoardService boardService;
+
+    @PostMapping
+    public ResponseEntity<Void> createBoard(@RequestBody BoardCreateRequest request) {
+        Long boardId = boardService.createBoard(request);
+        URI location = URI.create("/api/boards/" + boardId);
+        return ResponseEntity.created(location).build();
+    }
 }
