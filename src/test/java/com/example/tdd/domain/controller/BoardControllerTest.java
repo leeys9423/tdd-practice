13342dfe2903f2036d32c8d9065f63812ee5c29a
leeys9423 <leeys9423@gmail.com>
2025-01-11 20:27:49 +0900

    test: 게시판 글 등록 컨트롤러 테스트 추가

--- src/test/java/com/example/tdd/domain/controller/BoardControllerTest.java
+++ src/test/java/com/example/tdd/domain/controller/BoardControllerTest.java
@@ -1,7 +1,53 @@
 package com.example.tdd.domain.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.tdd.domain.dto.BoardCreateRequest;
+import com.example.tdd.domain.service.BoardService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class BoardControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private BoardService boardService;
+    
+    @DisplayName("게시글 생성 요청 성공시 201과 Location 헤더를 반환한다.")
+    @Test
+    void createBoard() throws Exception {
+        //given
+        BoardCreateRequest request = new BoardCreateRequest(
+                "제목", "내용", "이영섭", "123456!"
+        );
+
+        given(boardService.createBoard(any(BoardCreateRequest.class)))
+                .willReturn(1L);
+        
+        //when & then
+        mockMvc.perform(post("/api/boards")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/api/boards/1"));
+    }
 }
